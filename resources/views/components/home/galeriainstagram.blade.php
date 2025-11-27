a938de0743812a8d74672a93a4b085e5a06e2044
Erick <erick.tuhogarconsentido@gmail.com>
2025-11-27 14:32:24 -0600

    feat style: ✨ 💄 Actualización en datos y media
    ### Se requiere cambios en la actualización del contador para el próximo evento de Expo Premios, actualización en números de contacto y en imágenes del carrusel.
    
    ### Se modifico la fecha en el archivo JS timer y la leyenda del año en el componente timer.blade.
    En el componente de la galeria se actualizo la dirección del src apuntando a una nueva carpeta que estára en assets y no en storage
    En el componente form, se actualizo el numero de telefono
    
    ### Se afectaron los componentes que están en el home, se creo una nueva carpeta para las nuevas imágenes y se afecto el JS de timer que controla el contador
    
    ### No se requirio ejecución de comandos

--- resources/views/components/home/galeriainstagram.blade.php
+++ resources/views/components/home/galeriainstagram.blade.php
@@ -6,8 +6,10 @@
                 <div class="instagram-active owl-carousel">
                     @foreach (range(1, 9) as $number)
                         <div class="single-instagram">
-                            <img src="{{ asset('storage/images/galeria_insta/' . $number . '.jpg') }}" alt="">
-                            <a target="_blank" href="https://www.instagram.com/showroomdiodi/"><i class="ti-instagram"></i></a>
+                            {{-- <img src="{{ asset('storage/images/galeria_insta_2025/' . $number . '.jpg') }}" alt=""> --}}
+                            <img src="{{ asset('assets/galeria_insta_2025/' . $number . '.jpg') }}" alt="">
+                            {{-- <a target="_blank" href="https://www.instagram.com/showroomdiodi/"><i class="ti-instagram"></i></a> --}}
+                            <a target="_blank" href="#"><i class="ti-instagram"></i></a>
                         </div>
                     @endforeach
                 </div>
